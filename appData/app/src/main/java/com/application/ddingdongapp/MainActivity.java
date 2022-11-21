0f2e985858c178afee9ef3390c893cde554eb2d3
LIDAMDAM <phillip3652@gmail,com>
2022-11-21 19:28:26 +0900

    11.21 커밋 작성자 : 이다함 ———————— * 추가 -switch 관련 xml 추가 * 수정 -activity_main.xml 수정(ui 수정) -MainActivity.java 수정(배열 추가, 스피너 사용) -AndroidManifest.xml 수정(activity를 메인으로 연결) -edge.xml 수정(테두리 1로 수정) * 삭제 — login_activity.xml 파일 삭제

--- appData/app/src/main/java/com/application/ddingdongapp/MainActivity.java
+++ appData/app/src/main/java/com/application/ddingdongapp/MainActivity.java
@@ -3,20 +3,48 @@ package com.application.ddingdongapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.application.ddingdongapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+    String[] items = {"과목0", "과목1", "과목2", "과목3", "과목4"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.tvName.setText("이다함");
-        binding.swStartTime.setText("");
-        String items[] = {"아이템0", "아이템1", "아이템2", "아이템3", "아이템4"};
+        setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.swStartTime);
+        textView2 = findViewById(R.id.swSubmit);
+        textView3 = findViewById(R.id.swVideo);
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textView1.setText(items[position]);
+                textView2.setText(items[position]);
+                textView3.setText(items[position]);}
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                textView1.setText("");
+                textView2.setText("");
+                textView3.setText("");}});
 
     }
 }
