ffed54abbe99b2b9064631520f2fd9176a74e99f
LIDAMDAM <phillip3652@gmail,com>
2022-11-21 16:56:48 +0900

    11.21 커밋 작성자 : 이다함 ———————— * 수정 -activity_main.xml 수정(ut 수정) -MainActivity.java 수정(배열 추가) -AndroidManifest.xml 수정(activity를 메인으로 연결) -edge.xml 수정(테두리 1로 수정) * 삭제 — login_activity.xml 파일 삭제

--- appData/app/src/main/java/com/application/ddingdongapp/MainActivity.java
+++ appData/app/src/main/java/com/application/ddingdongapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.application.ddingdongapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.application.ddingdongapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.tvName.setText("이다함");
+        binding.swStartTime.setText("");
+        String items[] = {"아이템0", "아이템1", "아이템2", "아이템3", "아이템4"};
+
     }
 }
